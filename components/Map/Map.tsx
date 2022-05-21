d4cae41dbdc99b8318da77ab53328f7dbdbb102d
ev4ik94 <evelina9449@gmail.com>
2022-05-21 20:22:29 +0500

    ts fix err

--- components/Map/Map.tsx
+++ components/Map/Map.tsx
@@ -1,17 +1,19 @@
 import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
 
-function MapComponent(props){
+function MapComponent(props:any){
 
     const center = { lat: 41.49268754076146, lng: 69.48898361759296 };
     return(
         <div className='container position-relative' style={props.style}>
             <Map
                 google={props.google}
+                //@ts-ignore
                 zoom={14}
                 initialCenter={center}
                 className='position-relative'
 
             >
+                {/*@ts-ignore*/}
                 <Marker position={center} text='Global Solutions' />
             </Map>
         </div>
